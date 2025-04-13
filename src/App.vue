31af08f7b68d2d2a491c63c9e6f95c3bc39e6626
unknown <rizkify17@email.com>
2025-04-13 14:11:29 +0700

    Tambahan command

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import sidebar from './components/sidebar.vue'
 import { onMounted, ref } from 'vue'
 
-// Deklarasi untuk satuan skill
+// Deklarasi untuk section skill
 const skills = [
   { name: 'Figma', value: 75},
   { name: 'Javascript', value: 55},
@@ -13,9 +13,9 @@ const skills = [
   { name: 'Video Editing', value: 76},
   { name: 'Photography', value: 89},
 ]
-
+// reactive state untuk skills
 const displayedValues = ref(skills.map(() => 0))
-
+// Animasi masuk untuk section skils
 onMounted(() => {
   const observer = new IntersectionObserver(
     (entries) => {
@@ -38,11 +38,11 @@ onMounted(() => {
     },
     { threshold: 0.5 }
   )
-
+  // Menghubungkan section skill dengan observer
   const el = document.getElementById('skill')
   if (el) observer.observe(el)
 })
-
+// Deklarasi untuk isi section project
 const tools = [
   { name: 'Vite', src: '/public/vite.svg', glowColor: '#facc15', link: 'https://vitejs.dev/'},
   { name: 'Vue', src: '/public/vue.svg', glowColor: '#42b883', link: 'https://vuejs.org/'},
@@ -50,7 +50,7 @@ const tools = [
   { name: 'Node.js', src: '/public/nodejs-icon.svg', glowColor: '#68a063',  link: 'https://nodejs.org/'},
   { name: 'Git', src: '/public/git-scm-icon.svg', glowColor: '#f1502f', link: 'https://git-scm.com/'}
 ]
-
+// Animasi masuk untuk semua section kecuali skill
 onMounted(() => {
   const revealElements = document.querySelectorAll('.scroll-reveal')
 
@@ -74,9 +74,10 @@ onMounted(() => {
   })
 })
 </script>
+
 <template>
   <sidebar/>
-  <!-- style untuk setiap section -->
+  <!-- style untuk general -->
   <div class="bg-gray-900 text-white"> 
     <!-- Section Utama/Hero -->
     <section id="Hero" class="min-h-screen flex items-center justify-center bg-gray-900 text-white px-4">
@@ -96,8 +97,9 @@ onMounted(() => {
     </div>
       </div>
     </section>
-
+    <!-- Bagian style section about -->
     <section id="about" class="min-h-screen flex items-center justify-center bg-gray-800 text-white px-4 py-20 space-y-6 scroll-reveal opacity-0 translate-y-10 transition-all duration-500 ease-out">
+      <!-- Style untuk photo/gambar -->
       <div class="max-w-6x1 w-full grid md:grid-cols-2 gap-12 items-center">
         <div class="flex justify-center">
           <img
@@ -106,7 +108,7 @@ onMounted(() => {
             class="w-80 h-96 object-cover rounded-lg transition duration-400 hover:shadow-[0_1px_10px_5px_rgba(255,255,255,0.5)]"
             />
         </div>
-
+        <!-- Style untuk text -->
       <div class="space-y-6">
       <h2 class="text-indigo-400 text-xl font-medium">Discover</h2>
       <h1 class="text-4xl font-bold">About Me</h1>
@@ -116,7 +118,7 @@ onMounted(() => {
         Back-End developer, Linux Server, serta desain grafis, photography,
         videography dan UI/UX design.
       </p>
-
+      <!-- Syle untuk card -->
       <div class="grid grid-cols-2 gap-4 text-sm text-gray-200 border border-indigo-500 p-4 rounded-md">
         <p><span class="font-semibold">Name:</span> Rizki Kurniawan</p>
         <p><span class="font-semibold">Age:</span> 18</p>
@@ -125,7 +127,7 @@ onMounted(() => {
         <p><span class="font-semibold">School:</span> SMK N 2 Yogyakarta</p>
         <p><span class="font-semibold">Email:</span> rizkify17@email.com</p>
       </div>
-
+      <!-- Style untuk tombol download -->
       <button class="mt-4 px-6 py-2 bg-red-500 transition duration-400 hover:shadow-[0_1px_10px_5px_rgba(255,255,255,0.5)]">
         <a
         href="/public/CV-Rizki.pdf"
@@ -136,12 +138,13 @@ onMounted(() => {
       </div>
       </div>
     </section>
-
+    <!-- Bagian style section skill -->
     <section id="skill" class="min-h-[60vh] bg-gray-900 text-white px-4 py-20">
       <div class="text-center mb-12">
       <h2 class="text-4xl font-bold text-yellow-400">My Skills</h2>
     </div>
     <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 max-w-6xl mx-auto">
+      <!-- Pengambilan script dan style untuk card skill -->
       <div
         v-for="(skill, index) in skills"
         :key="skill.name"
@@ -154,15 +157,14 @@ onMounted(() => {
         </div>
       </div>
     </div>
-
     </section>
-
+    <!-- Bagian style section project -->
     <section id="project" class="min-h-[55vh] flex flex-col items-center justify-center bg-gray-800 space-y-6 scroll-reveal opacity-0 translate-y-10 transition-all duration-500 ease-out">
       <div class="text-center mb-12">
         <h2 class="text-4xl md:text-5xl font-bold text-yellow-400">In This Project</h2>
         <p class="text-gray-400 mt-2 text-lg">Tools & Frameworks</p>
       </div>
-
+      <!-- Pengambilan script logo dan style untuk logo -->
       <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-10 max-w-6xl mx-auto">
       <div
       v-for="(tool, index) in tools"
@@ -182,14 +184,14 @@ onMounted(() => {
       </div>
       </div>
     </section>
-
+    <!-- Bagian style section contact -->
     <section id="contact" class="min-h-screen bg-gray-900 text-white px-4 py-20 flex items-center justify-center space-y-6 scroll-reveal opacity-0 translate-y-10 transition-all duration-500 ease-out">
       <div class="w-full max-w-3xl text-center space-y-10">
         <div>
           <h2 class="text-4xl md:text-5xl font-bold text-yellow-400">Contact Me</h2>
           <p class="text-gray-400 mt-2 text-lg">Just say hi! 👋</p>
         </div>
-
+        <!-- Kolom untuk mengetik pesan -->
         <form class="space-y-6">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
           <input type="text" placeholder="Your Name" class="p-4 rounded-md bg-gray-800 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-yellow-400 transition duration-300" />
@@ -197,13 +199,14 @@ onMounted(() => {
         </div>
 
         <textarea placeholder="Your Message" rows="5" class="w-full p-4 rounded-md bg-gray-800 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-yellow-400 transition duration-300"></textarea>
-
+        <!-- Tombol untuk mengirim pesan tetapi belum bisa di gunakan -->
         <button type="submit" class="px-8 py-3 bg-yellow-400 text-black font-semibold rounded-md hover:shadow-[0_0_20px_5px_rgba(250,204,21,0.5)] transition duration-300">
         Send Message
         </button>
         </form>
       </div>
     </section>
+    <!-- Bagian style footer -->
     <footer class="bg-gray-900 py-10">
   <div class="flex justify-center items-center gap-10">
     <!-- GitHub logo -->
